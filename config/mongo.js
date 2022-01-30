628f738f839da0fd22d07db019a860fec2b9a720
John Koen <jkoen@smartronix.com>
2022-01-30 17:43:00 -0500

    Added basic user route for testing

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const DB_URL = process.env.MONGO_URI;
-console.log(process.env.PORT);
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {});
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const connectDB = require('./config/mongo');
 
 // Load env vars
@@ -8,7 +9,21 @@ dotenv.config();
 // Connect to database
 connectDB();
 
+// Routes
+const users = require('./routes/users');
+
 const app = express();
+
+// JSON parser
+app.use(express.json());
+
+// Mount routers
+app.use('/api/v1/users', users);
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 const PORT = process.env.PORT || 5000;
 
