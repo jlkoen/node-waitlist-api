4da124dee2eb10dd952506762bdf36851bcdb446
John Koen <jkoen@smartronix.com>
2022-01-30 22:21:08 -0500

    Added user GET and POST routes

--- controllers/users.js
+++ controllers/users.js
@@ -1,21 +1,36 @@
 const User = require('../models/User');
+const asyncHandler = require('../middleware/async');
 
 // @desc      Get all users
 // @route     GET /api/v1/users
 // @access    Private/Admin
 
-exports.getUsers = async (req, res) => {
-  try {
-    res.status(200).json({ success: true, msg: `Show all users ` });
-  } catch (error) {}
+exports.getUsers = async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({ success: true, count: users.length, data: users });
+  next();
 };
 
 // @desc      Get single user
 // @route     GET /api/v1/users:id
 // @access    Private/Admin
 
-exports.getUser = async (req, res) => {
-  try {
-    res.status(200).json({ success: true, msg: `Show user ${req.params.id}` });
-  } catch (error) {}
+exports.getUser = async (req, res, next) => {
+  const users = await User.findById(req.params.id);
+
+  res.status(200).json({ success: true, count: users.count, data: users });
+  next();
+};
+
+// @desc      Create user
+// @route     POST /api/v1/users
+// @access    Public
+
+exports.createUser = async (req, res, next) => {
+  console.log(req);
+  const users = await User.create(req.body);
+
+  res.status(201).json({ success: true, data: users });
+  next();
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
+const { getUsers, getUser, createUser } = require('../controllers/users');
+
 const router = express.Router();
 
-const { getUsers, getUser } = require('../controllers/users');
-
-router.get('/', getUsers);
-router.get('/:id', getUser);
+router.route('/').get(getUsers).post(createUser);
+router.route('/:id').get(getUser);
 
 module.exports = router;
